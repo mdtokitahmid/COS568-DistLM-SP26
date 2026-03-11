00bdeb8496710d9c1542a3d4b2d274dd8784584c
Toki64 <Toki64@node-0.mt3204-exp1-new.cos568proj2-sp26-pg0.wisc.cloudlab.us>
2026-03-11 06:52:20 -0500

    bug fixed 2a

--- task2a/run_glue.py
+++ task2a/run_glue.py
@@ -474,9 +474,13 @@ def main():
         global_step, tr_loss = train(args, train_dataset, model, tokenizer)
         logger.info(" global_step = %s, average loss = %s", global_step, tr_loss)
 
-    # Evaluation
-    if args.local_rank ==0:
+    if args.world_size > 1:
+        dist.barrier()
+    if args.local_rank == 0:
         evaluate(args, model, tokenizer, prefix="")
 
 if __name__ == "__main__":
     main()
+
+
+# python3 task2a/run_glue.py --model_type bert --model_name_or_path bert-base-cased   --task_name $TASK_NAME --do_train --do_eval   --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128   --per_device_train_batch_size 16 --learning_rate 2e-5   --num_train_epochs 1 --output_dir /tmp/$TASK_NAME/   --overwrite_output_dir   --master_ip 10.10.1.2 --master_port 12345 --world_size 4 --local_rank 0
