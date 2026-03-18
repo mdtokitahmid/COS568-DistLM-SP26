5ba2ce3126e5979278bc353364d82afe24daa98a
Toki64 <Toki64@node-0.mt3204-exp-final.cos568proj2-sp26-pg0.clemson.cloudlab.us>
2026-03-18 10:09:40 -0600

    update run_task4.sh with different ports and GLOO interface

--- run_task4.sh
+++ run_task4.sh
@@ -10,10 +10,10 @@ mkdir -p ~/COS568-DistLM-SP26/profiler_output
 # echo "=== Running Task 4 - 2a ==="
 # python3 task4/run_glue_2a.py --model_type bert --model_name_or_path bert-base-cased --task_name $TASK_NAME --do_train --do_eval --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_device_train_batch_size 16 --learning_rate 2e-5 --num_train_epochs 1 --output_dir /tmp/$TASK_NAME/ --overwrite_output_dir --master_ip 10.10.1.2 --master_port 12345 --world_size 4 --local_rank $RANK
 
-# echo "=== Running Task 4 - 2b ==="
-# python3 task4/run_glue_2b.py --model_type bert --model_name_or_path bert-base-cased --task_name $TASK_NAME --do_train --do_eval --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_device_train_batch_size 16 --learning_rate 2e-5 --num_train_epochs 1 --output_dir /tmp/$TASK_NAME/ --overwrite_output_dir --master_ip 10.10.1.2 --master_port 12340 --world_size 4 --local_rank $RANK
+echo "=== Running Task 4 - 2b ==="
+python3 task4/run_glue_2b.py --model_type bert --model_name_or_path bert-base-cased --task_name $TASK_NAME --do_train --do_eval --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_device_train_batch_size 16 --learning_rate 2e-5 --num_train_epochs 1 --output_dir /tmp/$TASK_NAME/ --overwrite_output_dir --master_ip 10.10.1.2 --master_port 12347 --world_size 4 --local_rank $RANK
 
-echo "=== Running Task 4 - 3 ==="
-python3 task4/run_glue_3.py --model_type bert --model_name_or_path bert-base-cased --task_name $TASK_NAME --do_train --do_eval --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_device_train_batch_size 16 --learning_rate 2e-5 --num_train_epochs 1 --output_dir /tmp/$TASK_NAME/ --overwrite_output_dir --master_ip 10.10.1.2 --master_port 12347 --world_size 4 --local_rank $RANK
+# echo "=== Running Task 4 - 3 ==="
+# python3 task4/run_glue_3.py --model_type bert --model_name_or_path bert-base-cased --task_name $TASK_NAME --do_train --do_eval --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_device_train_batch_size 16 --learning_rate 2e-5 --num_train_epochs 1 --output_dir /tmp/$TASK_NAME/ --overwrite_output_dir --master_ip 10.10.1.2 --master_port 12347 --world_size 4 --local_rank $RANK
 
 echo "=== Done ==="
